ac8b56924e9f2d604c943b9d1df1616655532e43
Pok <113675512+Pok27@users.noreply.github.com>
2025-12-02 21:39:21 +0200

    Move some admin components to shared (#41677)
    * AdminSystem-move-to-shared
    
    * review

--- Content.Shared/Administration/Systems/AdminGunSystem.cs
+++ Content.Shared/Administration/Systems/AdminGunSystem.cs
@@ -1,12 +1,7 @@
-// SPDX-FileCopyrightText: 2024 DrSmugleaf <DrSmugleaf@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
-//
-// SPDX-License-Identifier: MIT
-
-using Content.Server.Administration.Components;
+using Content.Shared.Administration.Components;
 using Content.Shared.Weapons.Ranged.Events;
 
-namespace Content.Server.Administration.Systems;
+namespace Content.Shared.Administration.Systems;
 
 public sealed class AdminGunSystem : EntitySystem
 {
